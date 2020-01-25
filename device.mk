683575c129b7c2b0fcd496ad981245cae7cb5ea6
argraur <argraur@gmail.com>
2020-01-25 22:27:42 +0530

    whyred: Add pixel charging animation
    Change-Id: Ib68836b54ad637ef9713ea9781ac68b8182ff0be
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- device.mk
+++ device.mk
@@ -335,6 +335,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Offline chargering animation
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
