fba663704ec525338339b1bc76f85272f4064022
SakilMondal <mondalsakil@gmail.com>
2021-01-21 12:56:16 +0530

    whyred: Drop non-functional soundtrigger configs
    * Fixes google assistant voice match in android 11
    
    Change-Id: I8d20c39f99ba871c327ab74fede4de721f1a4f33
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- device.mk
+++ device.mk
@@ -126,10 +126,7 @@ PRODUCT_COPY_FILES += \
 # Audio (Device Specific)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
-
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
